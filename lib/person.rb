a8e05afb909856a2b7c676ec540d2875f5360955
Jason Lewis <jasonedlewis@gmail.com>
2019-07-17 15:33:08 +0000

    Done.

--- lib/person.rb
+++ lib/person.rb
@@ -1,1 +1,26 @@
 # your code goes here
+
+class Person
+  attr_reader :name
+  attr_writer (:happiness, :hygiene)
+  attr_accessor :bank_account
+  
+  def initialize(name_arg)
+    @name = name_arg
+    @bank_account = 25
+    @happiness = 8
+    @hygiene = 8
+  end
+  
+  def clean?
+    @hygiene > 7
+  end
+  
+  def happy?
+    @happy > 7
+  end
+  
+end
+
+
+
