0d2e76b29effbd3afe92622d022f3946d2aa90d9
Jason Lewis <jasonedlewis@gmail.com>
2019-07-17 17:38:54 +0000

    Done.

--- lib/person.rb
+++ lib/person.rb
@@ -2,7 +2,6 @@
 
 class Person
   attr_reader :name, :happiness, :hygiene
-  attr_writer :happiness, :hygiene
   attr_accessor :bank_account
   
   def initialize(name_arg)
@@ -20,6 +19,24 @@ class Person
     @happiness > 7
   end
   
+  def happiness=(number)
+    @happiness = number_limits(number)
+    
+  end
+  def hygiene=(number)
+    @hygiene = number_limits(number)
+    
+  end
+  
+  def number_limits(number)
+    if number < 0
+      number = 0
+    elsif number > 10
+      number = 10
+    end
+    number
+  end
+  
   def get_paid(salary)
     @bank_account += salary
     "all about the benjamins"
@@ -27,26 +44,32 @@ class Person
   
   def take_bath
     @hygiene += 4
+    self.hygiene=(@hygiene)
     "♪ Rub-a-dub just relaxing in the tub ♫"
   end
   
   def work_out
     
     @happiness += 2
+    self.happiness=(@happiness)
+    
     @hygiene -= 3
+    self.hygiene=(@hygiene)
     "♪ another one bites the dust ♫"
   end
   
   def call_friend(friend)
     @happiness += 3
+    self.happiness=(@happiness)
     friend.happiness += 3
     "Hi #{friend.name}! It's #{self.name}. How are you?"
   end
   
   def start_conversation(person_arg, topic)
     if topic == "politics"
-      person_arg.happiness -= 1
-      self.happiness -= 1
+      person_arg.happiness -= 2
+      @happiness -= 2
+      self.happiness=(@happiness)
       return "blah blah partisan blah lobbyist"
       
     elsif topic == "weather"
@@ -60,8 +83,6 @@ class Person
     end
     
   end 
-  
-  
   
 end
 
